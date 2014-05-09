4899157ad4409b830c26ed7033e60bcb3b0fd464
llllllllll <joejev@gmail.com>
2014-05-08 22:17:46 -0400

    indentation

--- machine/register.h
+++ machine/register.h
@@ -21,45 +21,45 @@
 #include "../16common/common/arch.h"
 
 // Reserved Registers:
-extern c16_reg ipt;      // Instruction pointer.
-extern c16_reg spt;      // Stack pointer.
-extern c16_reg ac1;      // Accumulator register.
-extern c16_reg ac2;      // Second accumulator register.
-extern c16_reg tst;      // Comparison test register.
-extern c16_reg inp;      // Standard input buffer.
+extern c16_reg    ipt;   // Instruction pointer.
+extern c16_reg    spt;   // Stack pointer.
+extern c16_reg    ac1;   // Accumulator register.
+extern c16_reg    ac2;   // Second accumulator register.
+extern c16_reg    tst;   // Comparison test register.
+extern c16_reg    inp;   // Standard input buffer.
 extern c16_subreg inp_r; // The section to read from the input buffer.
 extern c16_subreg inp_w; // The section to write to the input buffer.
 
 // Free Registers:
-extern c16_reg r0;       // Free register 0.
+extern c16_reg    r0;    // Free register 0.
 extern c16_subreg r0_f;
 extern c16_subreg r0_b;
-extern c16_reg r1;       // Free register 1.
+extern c16_reg    r1;    // Free register 1.
 extern c16_subreg r1_f;
 extern c16_subreg r1_b;
-extern c16_reg r2;       // Free register 2.
+extern c16_reg    r2;    // Free register 2.
 extern c16_subreg r2_f;
 extern c16_subreg r2_b;
-extern c16_reg r3;       // Free register 3.
+extern c16_reg    r3;    // Free register 3.
 extern c16_subreg r3_f;
 extern c16_subreg r3_b;
-extern c16_reg r4;       // Free register 4.
+extern c16_reg    r4;    // Free register 4.
 extern c16_subreg r4_f;
 extern c16_subreg r4_b;
-extern c16_reg r5;       // Free register 5.
+extern c16_reg    r5;    // Free register 5.
 extern c16_subreg r5_f;
 extern c16_subreg r5_b;
-extern c16_reg r6;       // Free register 6.
+extern c16_reg    r6;    // Free register 6.
 extern c16_subreg r6_f;
 extern c16_subreg r6_b;
-extern c16_reg r7;       // Free register 7.
+extern c16_reg    r7;    // Free register 7.
 extern c16_subreg r7_f;
 extern c16_subreg r7_b;
-extern c16_reg r8;       // Free register 8.
+extern c16_reg    r8;    // Free register 8.
 extern c16_subreg r8_f;
 extern c16_subreg r8_b;
-extern c16_reg r9;       // Free register 9.
+extern c16_reg    r9;    // Free register 9.
 extern c16_subreg r9_f;
 extern c16_subreg r9_b;
 
-#endif /* REGISTER_H */
+#endif
