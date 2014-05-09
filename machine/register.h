2c067dc82358b76e32683b0a120ccc026c844331
llllllllll <joejev@gmail.com>
2014-05-08 22:34:05 -0400

    no need for common in machine

--- machine/register.h
+++ machine/register.h
@@ -18,8 +18,6 @@
 #ifndef C16_REGISTER_H
 #define C16_REGISTER_H
 
-#include "../16common/common/arch.h"
-
 // Reserved Registers:
 extern c16_reg    ipt;   // Instruction pointer.
 extern c16_reg    spt;   // Stack pointer.
